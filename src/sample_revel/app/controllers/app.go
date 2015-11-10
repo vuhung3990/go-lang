6beb0031c3d11a9c515b34301fd4e39cada89a12
Hung Vu <vuhung3990@gmail.com>
2015-11-10 11:52:53 +0700

    sample route

--- src/sample_revel/app/controllers/app.go
+++ src/sample_revel/app/controllers/app.go
@@ -9,3 +9,8 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+//note: name is same with view/App/Hello.html
+func (c App) Hello() revel.Result {
+	return c.Render()
+}
