b872001ae8dd2bb674fee9486dcd79232e559180
Hung Vu <vuhung3990@gmail.com>
2015-11-10 16:40:30 +0700

    sample post data and use efficiently concatenate strings

--- src/sample_revel/app/controllers/app.go
+++ src/sample_revel/app/controllers/app.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"github.com/revel/revel"
+	"bytes"
+)
 
 type App struct {
 	*revel.Controller
@@ -12,5 +15,23 @@ func (c App) Index() revel.Result {
 
 //note: name is same with view/App/Hello.html
 func (c App) Hello() revel.Result {
-	return c.Render()
+	return c.RenderText("hell")
+}
+
+// post data sample
+// note: every controller will start with Upper-Case
+func (c App) CreateAccount(email, password, name string, age int) revel.Result {
+	c.Validation.Email(email).Message("email not valid")
+	c.Validation.MinSize(password, 6).Message("password too short")
+
+	if(c.Validation.HasErrors()){
+		// if you want efficiently concatenate strings in Go, use buffer
+		// you can use a+= "string...."
+		var r bytes.Buffer
+		for _, v := range c.Validation.Errors {
+			r.WriteString(v.Message+"<br/>")
+		}
+		return c.RenderText(r.String())
+	}
+	return c.RenderText("success");
 }
