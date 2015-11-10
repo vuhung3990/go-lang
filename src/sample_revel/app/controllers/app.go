01975580a35f4ca54d79c5a84b2e7cce894fe204
Hung Vu <vuhung3990@gmail.com>
2015-11-10 17:05:35 +0700

    validations

--- src/sample_revel/app/controllers/app.go
+++ src/sample_revel/app/controllers/app.go
@@ -21,17 +21,22 @@ func (c App) Hello() revel.Result {
 // post data sample
 // note: every controller will start with Upper-Case
 func (c App) CreateAccount(email, password, name string, age int) revel.Result {
+	// validations
 	c.Validation.Email(email).Message("email not valid")
 	c.Validation.MinSize(password, 6).Message("password too short")
+	c.Validation.Required(name).Message("name is required")
+	c.Validation.Min(age, 3).Message("age too young, are you kidding me?")
 
-	if(c.Validation.HasErrors()){
+	if (c.Validation.HasErrors()) {
 		// if you want efficiently concatenate strings in Go, use buffer
 		// you can use a+= "string...."
+
 		var r bytes.Buffer
 		for _, v := range c.Validation.Errors {
-			r.WriteString(v.Message+"<br/>")
+			r.WriteString(v.Message + "<br/>")
 		}
 		return c.RenderText(r.String())
 	}
-	return c.RenderText("success");
+
+	return c.RenderText("success")
 }
