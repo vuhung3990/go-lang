80f80acb31eef87db3783a8807703fe21f1dc637
Hung Vu <vuhung3990@gmail.com>
2015-11-11 17:30:49 +0700

    config and get revel setting

--- src/sample_revel/app/controllers/app.go
+++ src/sample_revel/app/controllers/app.go
@@ -1,11 +1,16 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"github.com/revel/revel"
+)
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
+	// get constant from setting
+	revel.INFO.Println(revel.Config.StringDefault("db.database", "ban anh trien"))
+
 	return c.Render()
 }
